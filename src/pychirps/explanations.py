d2154d68a4d39ee41688100e3e533aff8308d317
hatvalues <125478155+hatvalues@users.noreply.github.com>
2024-11-11 10:29:19 +0000

    added parsing out final rules

--- src/pychirps/explanations.py
+++ src/pychirps/explanations.py
@@ -1,22 +1,33 @@
 from src.pychirps.build_rules.rule_utilities import NodePattern
-from src.pychirps.pandas_utils.data_encoding import PandasEncoder
+from functools import cached_property
 
 
 class RuleParser:
-    def __init__(self, encoder: PandasEncoder) -> None:
-        self.encoder = encoder
+    def __init__(self, feature_names: list[str]) -> None:
+        self.feature_names = feature_names
+        
+
+    @cached_property
+    def leq_segments(self):
+        return [{True: "<=", False: ">"} if feature_name.startswith("num__") else {True: "is not", False: "is"} for feature_name in self.feature_names]
+
+
+    def parse_leq(self, node: NodePattern) -> str:
+        return self.leq_segments[node.feature][node.leq_threshold]
 
     @staticmethod
-    def parse_leq(node: NodePattern) -> str:
-        if node.leq_threshold:
-            return "<="
-        return ">"
+    def parse_cat(node: tuple[str]) -> tuple[str]:
+        split_name = node[0].replace("cat__", "").split("_")
+        split_attrib = split_name.pop()
+        return tuple(["_".join(split_name), node[1], split_attrib])
 
-    def parse_feature(self, node: NodePattern) -> str:
-        return self.encoder.preprocessor.inverse_transform(node.feature)
-
-    def parse_rule(self, pattern: tuple[NodePattern], y_pred: int) -> str:
-        return [
-            f"{self.parse_feature(node)} {self.parse_leq(node)} {node.threshold}"
+    def parse(self, pattern: tuple[NodePattern], y_pred: int) -> str:
+        num_parse = [
+            (self.feature_names[node.feature].replace("num__", ""), self.parse_leq(node), node.threshold)
             for node in pattern
         ]
+        cat_parse = [self.parse_cat(node) if node[0].startswith("cat__") else node for node in num_parse]
+        final_parse = [" ".join(str(n) for n in node) for node in cat_parse]
+        return final_parse
+        
+
--- tests/test_explanations.py
+++ tests/test_explanations.py
@@ -1,20 +1,40 @@
 from src.pychirps.explanations import RuleParser
 from src.pychirps.build_rules.rule_utilities import NodePattern
+from tests.fixture_helper import assert_dict_matches_fixture
 
+def test_cat_parse(nursery_enc):
+    rule_parser = RuleParser(nursery_enc.encoder.preprocessor.get_feature_names_out().tolist())
+    assert rule_parser.parse_cat(("cat__field_value", "is", 0.5)) == ("field", "is", "value")
+    assert rule_parser.parse_cat(("field_name_value", "is", 0.5)) == ("field_name", "is", "value")
 
-def test_enc(cervicalb_enc):
-    print(cervicalb_enc.inverse_transform_features)
-    assert False
 
 def test_parse_rule_cervical(cervicalb_enc):
-    rule_parser = RuleParser(cervicalb_enc.encoder)
-
-    rule_parser.parse_rule(
+    rule_parser = RuleParser(cervicalb_enc.encoder.preprocessor.get_feature_names_out().tolist())
+    rule = rule_parser.parse(
         (
             NodePattern(0, 3.0, True),
             NodePattern(1, 3.5, False),
             NodePattern(9, 2.7, True),
             NodePattern(10, 22.1, False),
         ),
-        y_pred=1
+        y_pred=1)
+    assert_dict_matches_fixture({"rule": rule}, "rule_cervical_1")
+
+
+def test_parse_rule_nursery(nursery_enc):
+    rule_parser = RuleParser(nursery_enc.encoder.preprocessor.get_feature_names_out().tolist())
+
+    rule = rule_parser.parse(
+        (
+            NodePattern(0, 0.5, True),
+            NodePattern(1, 0.5, False),
+            NodePattern(9, 0.5, True),
+            NodePattern(10, 0.5, False),
+        ),
+        y_pred=3
     )
+    assert_dict_matches_fixture({"rule": rule}, "rule_nursery_1")
+
+
+
+
