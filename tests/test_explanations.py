17d26233bfc840adc03f14d6dcc422aeda8613b3
hatvalues <125478155+hatvalues@users.noreply.github.com>
2025-04-08 09:39:55 +0000

    delete not needed test

--- tests/test_explanations.py
+++ tests/test_explanations.py
@@ -4,23 +4,6 @@ from app.pychirps.rule_mining.rule_utilities import NodePattern
 from tests.fixture_helper import assert_dict_matches_fixture
 
 
-def test_cat_parse(nursery_pd, nursery_enc):
-    rule_parser = RuleParser(
-        nursery_enc.encoder.preprocessor.get_feature_names_out().tolist(),
-        nursery_pd.column_descriptors,
-    )
-    assert rule_parser.parse_cat(("cat__field_value", "is", 0.5)) == (
-        "field",
-        "is",
-        "value",
-    )
-    assert rule_parser.parse_cat(("field_name_value", "is", 0.5)) == (
-        "field_name",
-        "is",
-        "value",
-    )
-
-
 def test_parse_rule_cervical(cervicalb_pd, cervicalb_enc):
     rule_parser = RuleParser(
         cervicalb_enc.encoder.preprocessor.get_feature_names_out().tolist(),
